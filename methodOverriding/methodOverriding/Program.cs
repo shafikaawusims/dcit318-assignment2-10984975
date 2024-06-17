6a9eea10d2e4759bbca4124b1de5d0d604041f13
shafikaawusims <103181100+shafikaawusims@users.noreply.github.com>
2024-06-17 23:52:49 +0000

    Overriding

---
+++ methodOverriding/methodOverriding/Program.cs
@@ -0,0 +1,42 @@
+﻿// Base class
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+// Main method
+public class Program
+{
+    public static void Main()
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.MakeSound();  // Outputs: Some generic sound
+        dog.MakeSound();     // Outputs: Bark
+        cat.MakeSound();     // Outputs: Meow
+    }
+}
+
