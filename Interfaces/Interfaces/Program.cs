80814f131188dfd99b68eac6dc6cb92ea6ef6037
shafikaawusims <103181100+shafikaawusims@users.noreply.github.com>
2024-06-17 23:52:04 +0000

    Interfaces

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Interface
+public interface IMovable
+{
+    void Move();
+}
+
+// Class that implements IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class that implements IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+// Main method
+public class Program
+{
+    public static void Main()
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();  // Outputs: Car is moving
+        bicycle.Move();  // Outputs: Bicycle is moving
+    }
+}
+
